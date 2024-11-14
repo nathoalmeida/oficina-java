85203f11c1fab9d0f8942d9d887b19fca14c8b26
Nathália Zófoli <nathalia.oalmeida@gmail.com>
2024-11-14 13:23:21 -0300

    jogo

---
+++ pedrapapeltesoura/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    Jogo jogo = new Jogo();
+
+    jogo.rodaJogo();
+
+   
+    }
+
+}
---
+++ pedrapapeltesoura/src/Jogador.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Jogador {
+
+// ATRIBUTOS
+    private String nome;
+    private int jogada;
+    private int pontos;
+
+
+// CONSTRUTOR
+    public Jogador(String nome) {
+        this.nome = nome;
+        this.pontos = 0;
+    }
+
+// MÉTODOS DA CLASSE
+    public void fazJogada() {
+        Scanner scan = new Scanner(System.in);
+        int jogada = scan.nextInt();
+        this.jogada = jogada;
+    }
+
+// GETTERS E SETTERS
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getJogada() {
+        return this.jogada;
+    }
+
+    public void setJogada(int jogada) {
+        this.jogada = jogada;
+    }
+
+    public int getPontos() {
+        return this.pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+}
